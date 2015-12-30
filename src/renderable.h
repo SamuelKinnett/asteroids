5c875c71a29f15b2cd38e3ef1fb73f57642867aa
= <samuelkinnett96@googlemail.com>
2015-12-30 17:04:09 +0000

    Added vector class, renderable interface

---
+++ src/renderable.h
@@ -0,0 +1,16 @@
+//*****************************************************************************
+//An interface that all renderable items must inherit from
+//
+//Samuel Kinnett 2015
+//***************************************************************************** 
+
+#ifndef RENDERABLE_H
+#define RENDERABLE_H
+
+class Renderable {
+
+	public:
+		virtual void Render() = 0;
+};
+
+#endif
